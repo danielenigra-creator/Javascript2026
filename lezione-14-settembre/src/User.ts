eafc290a112709b939d5145ccf1cd0051ce4e2b4
Dani <daniele.nigra@edu-its.it>
2026-09-14 10:43:02 +0000

    Esercizio ripasso typescript

---
+++ lezione-14-settembre/src/User.ts
@@ -0,0 +1,8 @@
+/*export default function User(nome: string, eta: number) {
+  return "Ciao sono" + " " + nome + " e ho" + " " + eta + " " + "anni.";
+}
+*/
+export interface User {
+  nome: string;
+  eta: number;
+}
---
+++ lezione-14-settembre/src/matematica.ts
@@ -0,0 +1,5 @@
+//export perchè voglio visualizzarla dall'esterno.
+export function somma(a: number, b: number): number {
+  return a + b;
+}
+// :numer mi specifica cosa restituisce la mia funzione, in questo caso un numero.
