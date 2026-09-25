fecd1f82207c99a6117ebfad14b6aa53ca73fc03
daniele nigra <daniele.nigra@edu-its.it>
2026-09-25 10:46:29 +0200

    modifica roster

--- roster-atleti-starter/roster-atleti/src/state.ts
+++ roster-atleti-starter/roster-atleti/src/state.ts
@@ -7,25 +7,27 @@ import { renderAtleti } from "./render";
 let atleti: Atleta[] = carica();
 let filtroCorrente: Filtro = "tutti";
 
-export function aggiungiAtleta(nome: string, disciplina: string, foto: string): void {
+export function aggiungiAtleta(
+  nome: string,
+  disciplina: string,
+  foto: string,
+): void {
   // TODO: crea un nuovo Atleta (id univoco, inSquadra: false),
   // aggiungilo all'array e salva.
-  
-  atleti.push(
-    {
-      id: Date.now(), // id univoco basato sul timestamp  
-      nome: nome,
-      disciplina: disciplina,
-      foto: foto,
-      inSquadra: false
-    }
-  );
+
+  atleti.push({
+    id: Date.now(), // id univoco basato sul timestamp
+    nome: nome,
+    disciplina: disciplina,
+    foto: foto,
+    inSquadra: false,
+  });
   salva(atleti);
 }
 
 export function eliminaAtleta(id: number): void {
   // TODO: rimuovi l'atleta con quell'id e salva.
-  atleti= atleti.filter(atleta => atleta.id !== id);
+  atleti = atleti.filter((atleta) => atleta.id !== id);
   salva(atleti);
   renderAtleti();
 }
